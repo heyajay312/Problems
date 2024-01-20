ad2372a1cd394648a460bc5c8e49fda6c3d54e4a
Dev Anand <75121759+heydevanand@users.noreply.github.com>
2024-01-21 01:16:22 +0530

    Update max.c

--- max/max.c
+++ max/max.c
@@ -1,4 +1,3 @@
-#include <cs50.h>
 #include <stdio.h>
 
 int find_Max(int nums[]);
@@ -10,11 +9,8 @@ int main(void)
     numSize = get_int("Enter the number of values to find the maximum: ");
     int numbers[numSize];
 
-    // Get Numbers
-    for (int i = 0; i < numSize; i++)
-    {
-        numbers[i] = get_int("Number %i: ", i + 1);
-    }
+    // Get Numbers in an array 
+    TODO
 
     int Maximum = find_Max(numbers);          // Maximum Value
     printf("The max value is %i\n", Maximum); // Print Maximum Value
